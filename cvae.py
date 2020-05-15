30860e97adca1619d23885e53a805525383c768b
Knors <welf.knors@st.ovgu.de>
2020-05-15 10:53:16 +0200

    set up cvae

---
+++ cvae.py
@@ -0,0 +1,156 @@
+import tensorflow as tf
+import time
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import PIL
+import imageio
+import IPython
+from IPython import display
+
+from model import CVAE, reparameterize
+
+# ----------------------------------------------------------------------------------------------------------------------
+# PARAMS
+# ----------------------------------------------------------------------------------------------------------------------
+TRAIN_BUF = 60000
+BATCH_SIZE = 128
+
+TEST_BUF = 10000
+
+optimizer = tf.keras.optimizers.Adam(1e-4)
+epochs = 100
+latent_dim = 50
+num_examples_to_generate = 16
+
+# ----------------------------------------------------------------------------------------------------------------------
+# DATA
+# ----------------------------------------------------------------------------------------------------------------------
+# Load images
+(train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+
+# Normalizing the images to the range of [0., 1.]
+train_images /= 255.
+test_images /= 255.
+
+# Binarization
+train_images[train_images >= .5] = 1.
+train_images[train_images < .5] = 0.
+test_images[test_images >= .5] = 1.
+test_images[test_images < .5] = 0.
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# LOSS METHODS
+# ----------------------------------------------------------------------------------------------------------------------
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2. * np.pi)
+    return tf.reduce_sum(
+        -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
+        axis=raxis)
+
+
+@tf.function
+def compute_loss(model, x):
+    mean, logvar = model.encode(x)
+    z = reparameterize(mean, logvar)
+    x_logit = model.decode(z)
+
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+    logpz = log_normal_pdf(z, 0., 0.)
+    logqz_x = log_normal_pdf(z, mean, logvar)
+    return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+
+
+@tf.function
+def compute_apply_gradients(model, x, optimizer):
+    with tf.GradientTape() as tape:
+        loss = compute_loss(model, x)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# IMAGE METHODS
+# ----------------------------------------------------------------------------------------------------------------------
+def generate_and_save_images(model, epoch, test_input):
+    predictions = model.sample(test_input)
+    fig = plt.figure(figsize=(4,4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0], cmap='gray')
+        plt.axis('off')
+
+    # tight_layout minimizes the overlap between 2 sub-plots
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    # plt.show()
+    plt.close(fig)
+
+
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# TRAINING
+# ----------------------------------------------------------------------------------------------------------------------
+# keeping the random vector constant for generation (prediction) so
+# it will be easier to see the improvement.
+random_vector_for_generation = tf.random.normal(
+    shape=[num_examples_to_generate, latent_dim])
+model = CVAE(latent_dim)
+
+
+generate_and_save_images(model, 0, random_vector_for_generation)
+
+for epoch in range(1, epochs + 1):
+    start_time = time.time()
+    for train_x in train_dataset:
+        compute_apply_gradients(model, train_x, optimizer)
+    end_time = time.time()
+
+    if epoch % 1 == 0:
+        loss = tf.keras.metrics.Mean()
+        for test_x in test_dataset:
+            loss(compute_loss(model, test_x))
+        elbo = -loss.result()
+        display.clear_output(wait=False)
+        print('Epoch: {}, Test set ELBO: {}, '
+              'time elapse for current epoch {}'.format(epoch,
+                                                        elbo,
+                                                        end_time - start_time))
+        generate_and_save_images(
+            model, epoch, random_vector_for_generation)
+
+
+plt.imshow(display_image(epochs))
+plt.axis('off')  # Display images
+
+anim_file = 'cvae.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    last = -1
+    for i, filename in enumerate(filenames):
+        frame = 2*(i**0.5)
+        if round(frame) > round(last):
+            last = frame
+        else:
+            continue
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
+
+if IPython.version_info >= (6, 2, 0, ''):
+    display.Image(filename=anim_file)
---
+++ tensorboard_sandbox.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from pathlib import Path
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+# Download the data. The data is already divided into train and test.
+# The labels are integers representing classes.
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = \
+    fashion_mnist.load_data()
+
+# Names of the integer classes, i.e., 0 -> T-short/top, 1 -> Trouser, etc.
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Sets up a timestamped log directory.
+logdir = Path("logs/train_data/", datetime.now().strftime("%Y%m%d-%H%M%S"))
+# Creates a file writer for the log directory.
+file_writer = tf.summary.create_file_writer(str(logdir))
+
+with file_writer.as_default():
+    # Don't forget to reshape.
+    images = np.reshape(train_images[0:25], (-1, 28, 28, 1))
+    tf.summary.image("25 training data examples", images, max_outputs=25, step=0)
+
+
+def plot_to_image(figure):
+    """Converts the matplotlib plot specified by 'figure' to a PNG image and
+    returns it. The supplied figure is closed and inaccessible after this call."""
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    # Add the batch dimension
+    image = tf.expand_dims(image, 0)
+    return image
+
+
+def image_grid():
+    """Return a 5x5 grid of the MNIST images as a matplotlib figure."""
+    # Create a figure to contain the plot.
+    figure = plt.figure(figsize=(10, 10))
+    for i in range(25):
+        # Start next subplot.
+        plt.subplot(5, 5, i + 1, title=class_names[train_labels[i]])
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+
+    return figure
+
+
+# Prepare the plot
+figure = image_grid()
+# Convert to image and log
+with file_writer.as_default():
+    tf.summary.image("Training data", plot_to_image(figure), step=0)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
